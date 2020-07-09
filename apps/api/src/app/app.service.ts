1e90267f9d2bc0356928dc7b7886398f74979ad0
AlexAegis <alexaegis@gmail.com>
2020-07-09 19:41:37 +0200

    feat: create nx workspace

---
+++ apps/api/src/app/app.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@nestjs/common';
+import { Message } from '@state-of-statemanagers/api-interfaces';
+
+@Injectable()
+export class AppService {
+  getData(): Message {
+    return { message: 'Welcome to api!' };
+  }
+}
